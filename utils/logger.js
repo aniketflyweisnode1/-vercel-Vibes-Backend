689cc8b6cdfa0e8f87f90a24d248f691671a8611
unknown <node1@flyweis.technology>
2025-10-03 16:59:48 +0530

    set alls

--- utils/logger.js
+++ utils/logger.js
@@ -1,55 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-
 /**
- * Simple logging utility
+ * Simple console-only logging utility
  */
 class Logger {
-  constructor() {
-    this.logDir = path.join(__dirname, '../logs');
-    this.ensureLogDirectory();
-  }
-
-  ensureLogDirectory() {
-    if (!fs.existsSync(this.logDir)) {
-      fs.mkdirSync(this.logDir, { recursive: true });
-    }
-  }
-
   formatMessage(level, message, meta = {}) {
     const timestamp = new Date().toISOString();
     const metaStr = Object.keys(meta).length ? ` ${JSON.stringify(meta)}` : '';
     return `[${timestamp}] ${level.toUpperCase()}: ${message}${metaStr}`;
   }
 
-  writeToFile(filename, message) {
-    const logFile = path.join(this.logDir, filename);
-    fs.appendFileSync(logFile, message + '\n');
-  }
-
   info(message, meta = {}) {
     const formattedMessage = this.formatMessage('info', message, meta);
     console.log(formattedMessage);
-    this.writeToFile('app.log', formattedMessage);
   }
 
   error(message, meta = {}) {
     const formattedMessage = this.formatMessage('error', message, meta);
     console.error(formattedMessage);
-    this.writeToFile('error.log', formattedMessage);
   }
 
   warn(message, meta = {}) {
     const formattedMessage = this.formatMessage('warn', message, meta);
     console.warn(formattedMessage);
-    this.writeToFile('app.log', formattedMessage);
   }
 
   debug(message, meta = {}) {
     if ('development') {
       const formattedMessage = this.formatMessage('debug', message, meta);
       console.log(formattedMessage);
-      this.writeToFile('debug.log', formattedMessage);
     }
   }
 }
