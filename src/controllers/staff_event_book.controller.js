a3ca6a5c1990b4acc4c5071e0171de49e62758cc
node1 <node1@flyweis.technology>
2025-10-30 13:44:04 +0530

    set

--- src/controllers/staff_event_book.controller.js
+++ src/controllers/staff_event_book.controller.js
@@ -19,7 +19,24 @@ const createStaffEventBook = asyncHandler(async (req, res) => {
     };
 
     const staffEventBook = await StaffEventBook.create(staffEventBookData);
-    sendSuccess(res, staffEventBook, 'Staff event booking created successfully', 201);
+
+    // Try to fetch staff price based on staff_id and staff_category_id
+    let staffPrice = null;
+    if (staffEventBook.staff_id && staffEventBook.staff_category_id) {
+      const priceDoc = await StaffWorkingPrice.findOne({
+        staff_id: staffEventBook.staff_id,
+        staff_category_id: staffEventBook.staff_category_id,
+        status: true
+      });
+      staffPrice = priceDoc ? priceDoc.price : null;
+    }
+
+    const response = {
+      ...staffEventBook.toObject(),
+      staff_price: staffPrice
+    };
+
+    sendSuccess(res, response, 'Staff event booking created successfully', 201);
   } catch (error) {
     throw error;
   }
