e7ce1e30d8d7a749b0b41fe241dd56c4f2537e3d
node-4 <node4@flyweis.technology>
2026-01-09 12:10:21 +0530

    code on 9 january 2026

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const corsOptions = {
   origin: [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'https://vibes-mr.netlify.app',
